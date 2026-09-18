e4d28d5b9dc57e55b75fe27ada091cf49fcf8479
Frutiger <richardmsantana2006@gmail.com>
2026-09-18 14:40:34 -0300

    Adicionar arquivo de configuração do Firebase

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Configuração pública do Firebase para o Gerador PCP ESA.
+// Cole aqui o objeto firebaseConfig fornecido pelo Firebase Console.
+// Não coloque chaves de servidor/service account neste arquivo.
+window.ESA_FIREBASE_CONFIG = {
+  apiKey: "COLOQUE_SUA_API_KEY",
+  authDomain: "COLOQUE_SEU_PROJECT_ID.firebaseapp.com",
+  projectId: "COLOQUE_SEU_PROJECT_ID",
+  storageBucket: "COLOQUE_SEU_PROJECT_ID.firebasestorage.app",
+  messagingSenderId: "COLOQUE_SEU_MESSAGING_SENDER_ID",
+  appId: "COLOQUE_SEU_APP_ID"
+};
